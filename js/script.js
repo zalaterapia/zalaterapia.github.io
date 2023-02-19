c4c0ccb14a0a56af95cb0409ad27eac5732b7453
charlierobin <charlie@charlierobin.com>
2023-02-19 07:48:43 +0100

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,42 @@
+var captions = -1;
+var showing = false;
+
+$(function () 
+{
+    var video = document.querySelector('video');
+
+    var tracks = video.textTracks;
+
+    // console.log(tracks);
+
+    for (let i = 0; i < video.textTracks.length; i++) 
+    {
+        // video.textTracks[i].mode = "hidden";
+
+        // showing or disabled
+
+        // console.log(video.textTracks[i].mode);
+
+        if(video.textTracks[i].mode=="showing")
+        {
+            captions = i;
+            showing = true;
+        }
+    }
+
+    // console.log(captions);
+
+    $( "#subtitles" ).click(function() 
+    {
+        if(showing)
+        {
+            video.textTracks[captions].mode = "disabled";
+        }
+        else
+        {
+            video.textTracks[captions].mode = "showing";
+        }
+
+        showing = !showing;
+    });
+});
